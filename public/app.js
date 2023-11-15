8922ecfd6d43a4b99231954b0c7663fe13c057ae
Abby <avanooyen@wisc.edu>
2023-11-14 19:45:15 -0600

    added modal html

--- public/app.js
+++ public/app.js
@@ -1,3 +1,33 @@
 //Shopping cart html
 
 document.querySelector("#sc-price").innerHTML += ``;
+
+
+//variables associated with signing up  
+let signupbtn = document.querySelector('#signupbtn');
+let signup_modal = document.querySelector('#signup_modal');
+let signup_modalbg = document.querySelector('#signup_modalbg');
+
+//variables associated with signing in  
+let signinbtn = document.querySelector('#signinbtn');
+let signin_modal = document.querySelector('#signin_modal');
+let signin_modalbg = document.querySelector('#signin_modalbg');
+
+
+// sign-up modal link
+signupbtn.addEventListener('click', () => {
+    signup_modal.classList.add('is-active');
+});
+// sign-up modal close
+signup_modalbg.addEventListener('click', () => {
+    signup_modal.classList.remove('is-active');
+});
+
+// sign-in modal link
+signinbtn.addEventListener('click', () => {
+    signin_modal.classList.add('is-active');
+})
+// sign-in modal close
+signin_modalbg.addEventListener('click', () => {
+    signin_modal.classList.remove('is-active');
+});
