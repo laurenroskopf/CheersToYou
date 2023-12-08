a5b75f84efc6115714e89ac747997959cc40c76e
Lauren <lauren.roskopf@gmail.com>
2023-12-07 21:10:40 -0600

    testing

--- public/testing.js
+++ public/testing.js
@@ -2,38 +2,59 @@
 const puppeteer = require("puppeteer");
 
 async function go() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 15,
+  });
 
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 15,
-    });
+  const page = await browser.newPage();
 
-    const page = await browser.newPage();
+  //access the site to be tested
 
-    //access the site to be tested 
+  await page.goto("https://cheers-to-you-eaa8c.firebaseapp.com/");
 
-    await page.goto("https://cheers-to-you-eaa8c.firebaseapp.com/");
+  //user click the sign-in button
 
-    //user click the sign-in button 
+  await page.click("#signupbtn");
 
-    await page.click("#signinbtn");
+  //user twill provide email/password to sign up
 
-    //user twill provide email/password to sign in 
+  await page.type("#firstname", "Lauren");
+  await page.type("#lastname", "Roskopf");
+  await page.type("#email", "test@test.com");
+  await page.type("#phonenumber", "2621112222");
+  await page.type("#password", "test1234");
 
-    await page.type("#email_", "test@test.com");
-    await page.type("#password_", "test1234");
+  //user clicks on the submit button
 
-    //user clicks on the submit button 
+  await page.click(
+    "#signup_form > div.field.is-grouped > div.control > button"
+  );
 
-    await page.click("#signin_form > div.field.is-grouped > div.control > button");
+  //set 2 delay
+  await new Promise((r) => setTimeout(r, 2000));
 
-    //test the search functionality 
+  //test clicking on product
+  await page.click("#pennantspg");
 
-    //set 2 delay 
-    // await new Promise((r) => setTimeout(r, 2000));
+  //  //set 2 delay
+  await new Promise((r) => setTimeout(r, 2000));
+  //   //selecting options for pennant
 
+  await page.type("#penPersonal", "Testing");
 
-    browser.close();
+  //   //submit order
+  await page.click("#addPennant");
+  //set 2 delay
+  await new Promise((r) => setTimeout(r, 2000));
+
+  //checking shopping cart
+  await page.click("#shoppingCart > i");
+
+  //delete item fron shopping cart
+  await page.click("#cart > div:nth-child(1) > div.is-clickable > i");
+
+  browser.close();
 }
 
 go();
